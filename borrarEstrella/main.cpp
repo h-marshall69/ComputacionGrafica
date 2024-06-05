49a8dd28b33581d255313ebc3df912108c582af4
Marshall <h.jermaine.lee@gmail.com>
2024-06-04 21:52:55 -0300

    add ViajeEspacio

---
+++ borrarEstrella/main.cpp
@@ -0,0 +1,67 @@
+#include <graphics.h>
+#include <cstdlib>
+#include <ctime>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int NUM_STARS = 100;
+const float SPEED = 10.0;
+
+struct Star {
+    float x, y;
+    float z;
+};
+
+void initializeStars(Star stars[], int numStars) {
+    for (int i = 0; i < numStars; ++i) {
+        stars[i].x = (rand() % WIDTH) - WIDTH / 2;
+        stars[i].y = (rand() % HEIGHT) - HEIGHT / 2;
+        stars[i].z = rand() % WIDTH;
+    }
+}
+
+void updateStars(Star stars[], int numStars, float speed) {
+    for (int i = 0; i < numStars; ++i) {
+        stars[i].z -= speed;
+        if (stars[i].z < 1) {
+            stars[i].x = (rand() % WIDTH) - WIDTH / 2;
+            stars[i].y = (rand() % HEIGHT) - HEIGHT / 2;
+            stars[i].z = WIDTH;
+        }
+    }
+}
+
+void drawStars(Star stars[], int numStars) {
+    for (int i = 0; i < numStars; ++i) {
+        float sx = stars[i].x / stars[i].z * WIDTH + WIDTH / 2;
+        float sy = stars[i].y / stars[i].z * HEIGHT + HEIGHT / 2;
+        float brightness = 1 - stars[i].z / WIDTH;
+        int color = 15 * brightness;
+        setcolor(color);
+        setfillstyle(SOLID_FILL, color);
+        fillellipse(sx, sy, 2, 2);  // draw a small circle instead of a pixel
+    }
+}
+
+int main() {
+    initwindow(WIDTH, HEIGHT, "Space Travel Simulation");
+
+    Star stars[NUM_STARS];
+    initializeStars(stars, NUM_STARS);
+
+    while (true) {
+        cleardevice();
+
+        updateStars(stars, NUM_STARS, SPEED);
+        drawStars(stars, NUM_STARS);
+
+        delay(10);
+
+        if (kbhit()) {
+            if (getch() == 27) break; // Escape key to exit
+        }
+    }
+
+    closegraph();
+    return 0;
+}
