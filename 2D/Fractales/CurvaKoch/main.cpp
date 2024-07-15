1ead480920c0a70486d0cd34392235732ebe948d
Marshall <h.jermaine.lee@gmail.com>
2024-07-15 18:36:46 -0300

    create H

---
+++ 2D/Fractales/CurvaKoch/main.cpp
@@ -0,0 +1,53 @@
+#include <graphics.h>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void drawKochCurve(int depth, int x1, int y1, int x2, int y2) {
+    if (depth == 0) {
+        line(x1, y1, x2, y2);
+    } else {
+        int deltaX = x2 - x1;
+        int deltaY = y2 - y1;
+
+        int x3 = x1 + deltaX / 3;
+        int y3 = y1 + deltaY / 3;
+
+        int x4 = x1 + 2 * deltaX / 3;
+        int y4 = y1 + 2 * deltaY / 3;
+
+        int xMid = (x1 + x2) / 2;
+        int yMid = (y1 + y2) / 2;
+
+        int x5 = (x3 + x4) / 2 + (y3 - y4) * sqrt(3) / 6;
+        int y5 = (y3 + y4) / 2 + (x4 - x3) * sqrt(3) / 6;
+
+        drawKochCurve(depth - 1, x1, y1, x3, y3);
+        drawKochCurve(depth - 1, x3, y3, x5, y5);
+        drawKochCurve(depth - 1, x5, y5, x4, y4);
+        drawKochCurve(depth - 1, x4, y4, x2, y2);
+    }
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    int depth = 2; // Profundidad de la curva de Koch
+
+    // Coordenadas iniciales para la línea base de la curva de Koch
+    int x1 = 100, y1 = 300;
+    int x2 = 700, y2 = 300;
+
+    drawKochCurve(depth, x1, y1, x2, y2);
+
+    while (!kbhit()) {
+        delay(200);
+    }
+
+    closegraph();
+    return 0;
+}
---
+++ 3D/H/main.cpp
@@ -0,0 +1,245 @@
+#include <graphics.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Point3D{
+public:
+    double x;
+    double y;
+    double z;
+
+    //Point3D() : x(0), y(0), z(0) {}
+    Point3D(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
+
+    void rotateX(int cx, int cy, int cz, double angle) {
+        double temp = ((y - cy)*cos(angle * M_PI / 180) - (z - cz)*sin(angle * M_PI / 180)) + cy;
+        z = ((y - cy)*sin(angle * M_PI / 180) + (z - cz)*cos(angle * M_PI / 180)) + cz;
+        y = temp;
+    }
+
+    void rotateY(int cx, int cy, int cz, double angle) {
+        double temp = ((x - cx)*cos(angle * M_PI / 180) + (z - cz)*sin(angle * M_PI / 180)) + cx;
+        z = (-(x - cx)*sin(angle * M_PI / 180) + (z - cz)*cos(angle * M_PI / 180)) + cz;
+        x = temp;
+    }
+
+    void rotateZ(int cx, int cy, int cz, double angle) {
+        double temp = ((x - cx)*cos(angle * M_PI / 180) - (y - cy)*sin(angle * M_PI / 180)) + cx;
+        y = ((x - cx)*sin(angle * M_PI / 180) + (y - cy)*cos(angle * M_PI / 180)) + cy;
+        x = temp;
+    }
+};
+
+class StarOne {
+public:
+    Point3D a;
+    Point3D b;
+    Point3D c;
+    Point3D d;
+    Point3D e;
+
+    Point3D o;
+    Point3D p;
+    Point3D q;
+    Point3D r;
+    Point3D s;
+
+    StarOne(int cx, int cy, int radius) {
+        double angle = 72.0;
+        a = Point3D(cx + radius*cos(angle * 1 * M_PI / 180), cy + radius*sin(angle * 1 * M_PI / 180), 0);
+        b = Point3D(cx + radius*cos(angle * 2 * M_PI / 180), cy + radius*sin(angle * 2 * M_PI / 180), 0);
+        c = Point3D(cx + radius*cos(angle * 3 * M_PI / 180), cy + radius*sin(angle * 3 * M_PI / 180), 0);
+        d = Point3D(cx + radius*cos(angle * 4 * M_PI / 180), cy + radius*sin(angle * 4 * M_PI / 180), 0);
+        e = Point3D(cx + radius*cos(angle * 5 * M_PI / 180), cy + radius*sin(angle * 5 * M_PI / 180), 0);
+
+        o = Point3D(cx + radius*cos(angle * 1 * M_PI / 180), cy + radius*sin(angle * 1 * M_PI / 180), radius/2);
+        p = Point3D(cx + radius*cos(angle * 2 * M_PI / 180), cy + radius*sin(angle * 2 * M_PI / 180), radius/2);
+        q = Point3D(cx + radius*cos(angle * 3 * M_PI / 180), cy + radius*sin(angle * 3 * M_PI / 180), radius/2);
+        r = Point3D(cx + radius*cos(angle * 4 * M_PI / 180), cy + radius*sin(angle * 4 * M_PI / 180), radius/2);
+        s = Point3D(cx + radius*cos(angle * 5 * M_PI / 180), cy + radius*sin(angle * 5 * M_PI / 180), radius/2);
+    }
+
+    void draw() {
+        setcolor(5);
+        line(a.x, a.y, c.x, c.y);
+        line(b.x, b.y, d.x, d.y);
+        line(c.x, c.y, e.x, e.y);
+        line(d.x, d.y, a.x, a.y);
+        line(e.x, e.y, b.x, b.y);
+
+        line(o.x, o.y, q.x, q.y);
+        line(p.x, p.y, r.x, r.y);
+        line(q.x, q.y, s.x, s.y);
+        line(r.x, r.y, o.x, o.y);
+        line(s.x, s.y, p.x, p.y);
+
+        line(o.x, o.y, a.x, a.y);
+        line(p.x, p.y, b.x, b.y);
+        line(q.x, q.y, c.x, c.y);
+        line(r.x, r.y, d.x, d.y);
+        line(s.x, s.y, e.x, e.y);
+
+        setfillstyle(SOLID_FILL, 10);
+        fillellipse(a.x, a.y, 4, 4);
+        fillellipse(b.x, b.y, 4, 4);
+        fillellipse(c.x, c.y, 4, 4);
+        fillellipse(d.x, d.y, 4, 4);
+        fillellipse(e.x, e.y, 4, 4);
+
+        fillellipse(o.x, o.y, 4, 4);
+        fillellipse(p.x, p.y, 4, 4);
+        fillellipse(q.x, q.y, 4, 4);
+        fillellipse(r.x, r.y, 4, 4);
+        fillellipse(s.x, s.y, 4, 4);
+    }
+
+    void rotateY(int cx, int cy, int cz, int angle) {
+        a.rotateY(cx, cy, cz, angle);
+        b.rotateY(cx, cy, cz, angle);
+        c.rotateY(cx, cy, cz, angle);
+        d.rotateY(cx, cy, cz, angle);
+        e.rotateY(cx, cy, cz, angle);
+
+        o.rotateY(cx, cy, cz, angle);
+        p.rotateY(cx, cy, cz, angle);
+        q.rotateY(cx, cy, cz, angle);
+        r.rotateY(cx, cy, cz, angle);
+        s.rotateY(cx, cy, cz, angle);
+    }
+
+    void rotateX(int cx, int cy, int cz, int angle) {
+        a.rotateX(cx, cy, cz, angle);
+        b.rotateX(cx, cy, cz, angle);
+        c.rotateX(cx, cy, cz, angle);
+        d.rotateX(cx, cy, cz, angle);
+        e.rotateX(cx, cy, cz, angle);
+
+        o.rotateX(cx, cy, cz, angle);
+        p.rotateX(cx, cy, cz, angle);
+        q.rotateX(cx, cy, cz, angle);
+        r.rotateX(cx, cy, cz, angle);
+        s.rotateX(cx, cy, cz, angle);
+    }
+
+    void rotateZ(int cx, int cy, int cz, int angle) {
+        a.rotateZ(cx, cy, cz, angle);
+        b.rotateZ(cx, cy, cz, angle);
+        c.rotateZ(cx, cy, cz, angle);
+        d.rotateZ(cx, cy, cz, angle);
+        e.rotateZ(cx, cy, cz, angle);
+
+        o.rotateZ(cx, cy, cz, angle);
+        p.rotateZ(cx, cy, cz, angle);
+        q.rotateZ(cx, cy, cz, angle);
+        r.rotateZ(cx, cy, cz, angle);
+        s.rotateZ(cx, cy, cz, angle);
+    }
+};
+
+class Hache{
+    public:
+    Point3D a[12];
+    Point3D b[12];
+    int n = 4;
+
+    Hache(int cx, int cy, int radius) {
+        double angle = 72.0;
+        a[0] = Point3D(cx - radius, cy - radius, 0);
+        a[1] = Point3D(cx + radius, cy - radius, 0);
+        a[2] = Point3D(cx + radius, cy + radius, 0);
+        a[3] = Point3D(cx - radius, cy + radius, 0);
+
+        b[0] = Point3D(cx - radius, cy - radius, radius/2);
+        b[1] = Point3D(cx + radius, cy - radius, radius/2);
+        b[2] = Point3D(cx + radius, cy + radius, radius/2);
+        b[3] = Point3D(cx - radius, cy + radius, radius/2);
+    }
+
+    void draw() {
+        setcolor(5);
+        for(int i = 1; i < n; i++) {
+            line(a[i - 1].x, a[i - 1].y, a[i].x, a[i].y);
+            line(b[i - 1].x, b[i - 1].y, b[i].x, b[i].y);
+        }
+
+        for(int i = 0; i < n; i++) {
+            fillellipse(a[i].x, a[i].y, 4, 4);
+            fillellipse(b[i].x, b[i].y, 4, 4);
+        }
+    }
+    void rotateY(int cx, int cy, int cz, int angle) {
+        for(int i = 0; i < n; i++) {
+            a[i].rotateY(cx, cy, cz, angle);
+            b[i].rotateY(cx, cy, cz, angle);
+        }
+    }
+
+    void rotateX(int cx, int cy, int cz, int angle) {
+        for(int i = 0; i < n; i++) {
+            a[i].rotateX(cx, cy, cz, angle);
+            b[i].rotateX(cx, cy, cz, angle);
+        }
+    }
+
+    void rotateZ(int cx, int cy, int cz, int angle) {
+        for(int i = 0; i < n; i++) {
+            a[i].rotateZ(cx, cy, cz, angle);
+            b[i].rotateZ(cx, cy, cz, angle);
+        }
+    }
+};
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    int centerX = getmaxx() / 2;
+    int centerY = getmaxy() / 2;
+
+    StarOne h(centerX, centerY, 200);
+    Hache uno(centerX, centerY, 100);
+
+    bool pivot = false;
+
+    while (true) {
+        cleardevice();
+        outtextxy(getmaxx() - 120, getmaxy() - 130, "Star...");
+        outtextxy(getmaxx() - 120, getmaxy() - 110, "Rotation : A or B");
+        outtextxy(getmaxx() - 120, getmaxy() - 90, "Rotation : W or S");
+        outtextxy(getmaxx() - 120, getmaxy() - 70, "Rotation : Q or E");
+        outtextxy(getmaxx() - 120, getmaxy() - 50, "Pause : X");
+        outtextxy(getmaxx() - 120, getmaxy() - 30, "Continue : Z");
+
+        if (kbhit()) {
+            char key = getch();
+            if (key == 'a' || key == 'A') {
+                uno.rotateY(centerX, centerY, 0, -1);
+            } else if (key == 'd' || key == 'd') {
+                uno.rotateY(centerX, centerY, 0, 1);
+            }else if (key == 'w' || key == 'W') {
+                uno.rotateX(centerX, centerY, 0, 1);
+            }else if (key == 's' || key == 'S') {
+                uno.rotateX(centerX, centerY, 0, -1);
+            }else if (key == 'q' || key == 'Q') {
+                uno.rotateZ(centerX, centerY, 0, 1);
+            }else if (key == 'e' || key == 'E') {
+                uno.rotateZ(centerX, centerY, 0, -1);
+            }else if (key == 'z' || key == 'Z') {
+                pivot = true;
+            }else if (key == 'x' || key == 'X') {
+                pivot = false;
+            } else {
+                closegraph();
+            }
+        }
+
+        uno.draw();
+        if(pivot)
+            uno.rotateY(centerX, centerY, 0, 1);
+        delay(16);
+    }
+    getch();
+    closegraph();
+    return 0;
+}
